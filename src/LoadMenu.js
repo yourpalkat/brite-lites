c1161ec6ad66c2355fc12b928950e801aa76ee64
derekmurr <derekmurr@gmail.com>
2019-05-31 16:29:42 -0400

    css organized more better

--- src/LoadMenu.js
+++ src/LoadMenu.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import LoadListItem from './LoadListItem.js'
 
+// this component builds the load selection menu. It receives its own isHidden state and the toggleModal method 
+// from Header.js, the DrawGrid method of App.js and the loadArray global state of App.js passed to it via props from Header.js
 class LoadMenu extends Component {
   constructor(props){
     super(props);
   }
 
+  // click handler for 'cancel' button hides this component by calling the toggleModal method of Header.js
   closeMenu = () => {
     this.props.toggleLoad('load');
   }
 
+  // builds the list of clickable items to show representing all available pictures to load
   makeOptionList = () => {
+    // map through the global array of loadable items; for each object in the array
     let optionList = this.props.loadArray.map((pictureObject, index) => {
+      // pass that object's name, grid data and this component's closeMenu method to a LoadListItem component as props
       return(
         <LoadListItem key={'ll'+ index} name={pictureObject.pictureName} drawGrid={this.props.drawGrid} pictureGrid={pictureObject.pictureGrid} closeMenu={this.closeMenu} />
       )
--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Bulb from './Bulb.js';
 
+// this component displays the clickable picture grid of bulbs
+// it receives the global selectedColor state, the global gridArray (which holds the color of every bulb Component)
+// and the global updateArrayColor method passed to it by props from App.js
 class Main extends Component {
   constructor(props) {
     super(props)
@@ -11,7 +14,9 @@ class Main extends Component {
   makeGrid = () => {
     let displayArray = this.props.gridArray.map((row, rowIndex) => {
       return (row.map((column, colIndex) => {
+        // keyString is just a unique id for each component based on its x,y value in the gridArray
         const keyString = rowIndex + "-" + colIndex;
+        // isOdd is for styling, it handles the offset-grid appearance: odd row are shifted to the left, even to the right
         const isOdd = rowIndex % 2;
         return (
           <Bulb key={keyString} row={rowIndex} column={colIndex} selColor={this.props.selectedColor} assignedColor={this.props.gridArray[rowIndex][colIndex]} updateArrayColor={this.props.updateArrayColor} isOdd={isOdd} />
